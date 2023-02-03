b67402311d786c627422f9e6a09de42566f8948d
Zdravko Dimitrov <z.dimitrov9626@gmail.com>
2023-02-03 15:27:36 +0200

    new course

--- SoftUni JS Tasks/JS Fundamental Tasks/test.js
+++ SoftUni JS Tasks/JS Fundamental Tasks/test.js
@@ -2,3 +2,7 @@ function test(name, age) {
   console.log(`Hello my name is ${name} and I'm ${age} years old`);
 }
 test("Pesho", 3);
+
+let multiplay = function multiply(a,b){
+  console.log(a * b);
+}

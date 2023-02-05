1793b1c285b5897cfc797ed56b2faa70a6a26c03
Zdravko Dimitrov <z.dimitrov9626@gmail.com>
2023-02-05 10:11:22 +0200

    new lecture

---
+++ SoftUni JS Tasks/JS Fundamental Tasks/Functions/repeatString.js
@@ -0,0 +1,5 @@
+function testSum(a, b) {
+  console.log(a + b);
+}
+let result = testSum(5, 3);
+console.log(result);
--- SoftUni JS Tasks/JS Fundamental Tasks/test.js
+++
@@ -1,8 +0,0 @@
-function test(name, age) {
-  console.log(`Hello my name is ${name} and I'm ${age} years old`);
-}
-test("Pesho", 3);
-
-let multiplay = function multiply(a,b){
-  console.log(a * b);
-}

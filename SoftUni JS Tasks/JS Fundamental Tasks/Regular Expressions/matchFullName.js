6032495747a9cc2bbeb3b07846097965d2ca32ac
Zdravko Dimitrov <z.dimitrov9626@gmail.com>
2023-03-16 21:50:37 +0200

    new lecture

---
+++ SoftUni JS Tasks/JS Fundamental Tasks/Regular Expressions/matchFullName.js
@@ -0,0 +1,7 @@
+function matchFullName(text){
+let regex = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/g;
+let result = text.match(regex);
+console.log(result.join(' '));
+
+}
+matchFullName("Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan  Ivanov");

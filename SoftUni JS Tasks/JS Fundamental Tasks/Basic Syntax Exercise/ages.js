650fa119cd93165ca66e5c85d8b5e0979d8a47f7
Zdravko Dimitrov <z.dimitrov9626@gmail.com>
2023-04-28 17:43:38 +0300

    new exersices

---
+++ SoftUni JS Tasks/JS Fundamental Tasks/Basic Syntax Exercise/ages.js
@@ -0,0 +1,16 @@
+function ages(num) {
+  if (num < 0) {
+    console.log("out of bounds");
+  } else if (num >= 0 && num <= 2) {
+    console.log("baby");
+  } else if (num >= 3 && num <= 13) {
+    console.log("child");
+  } else if (num >= 14 && num <= 19) {
+    console.log("teenager");
+  } else if (num >= 20 && num <= 65) {
+    console.log("adult");
+  } else {
+    console.log("elder");
+  }
+}
+ages(-1);
---
+++ SoftUni JS Tasks/JS Fundamental Tasks/Basic Syntax Exercise/rounding.js
@@ -0,0 +1,9 @@
+function rounding(x, y){
+ x = x.toFixed(y)
+
+  console.log(`${x.parseFloat(y)}`);
+  
+
+}
+
+rounding(3.51513, 2);
